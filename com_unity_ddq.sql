65e6b32fa5831a6dc977a3159bbcc69b49f4762a
Ken Nguyen <aznboikenny@yahoo.com>
2020-03-27 20:08:58 -0500

    Added ddq for database

---
+++ com_unity_ddq.sql
@@ -0,0 +1,36 @@
+-- Data Definition File for Com_Unity
+
+-- Reset Database
+SET FOREIGN_KEY_CHECKS = 0;
+DROP SCHEMA IF EXISTS `com_unity`;
+CREATE SCHEMA `com_unity`;
+USE `com_unity`;
+-- DROP TABLE IF EXISTS `Messages`;
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+-- Message meta data
+CREATE TABLE Messages (
+  message_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+  message_date DATETIME NOT NULL,
+  mood VARCHAR(191) NOT NULL,
+  name VARCHAR(255),
+  age INT,
+  location VARCHAR(255)
+)ENGINE=InnoDB;
+
+
+-- Message voice recording
+CREATE TABLE Voices (
+  message_id INT PRIMARY KEY UNIQUE NOT NULL,
+  voice_message MEDIUMBLOB NOT NULL,
+  FOREIGN KEY (message_id) REFERENCES Messages(message_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+)ENGINE=InnoDB;
+
+
+-- Set database's default charset to handle emojis
+ALTER DATABASE com_unity CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Set table's default charset to handle emojis
+ALTER TABLE Messages CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
