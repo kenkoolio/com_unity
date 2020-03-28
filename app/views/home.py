33a5115dd41e2c8604e13358326cf695fbdf4db9
Ken Nguyen <aznboikenny@yahoo.com>
2020-03-28 02:54:37 -0500

    working on saving message route

--- app/views/home.py
+++ app/views/home.py
@@ -2,8 +2,9 @@
 # Description: Defines routes for the home page.
 
 from __main__ import app
-from flask import render_template
+from flask import render_template, request, redirect
 import os, sys
+import datetime
 
 # Following Lines are for assigning parent directory dynamically.
 # Citation: https://askubuntu.com/questions/1163847/modulenotfounderror-while-importing-module-from-parent-folder
@@ -23,5 +24,57 @@ from db_connector.db_connector import connect_to_database, execute_query
 #     return render_template('test.html')
 
 @app.route('/')
-def test():
+def home():
+    '''
+    Serves home page
+    '''
     return render_template('index.html')
+
+
+@app.route('/messages', methods=['POST'])
+def save_message():
+    '''
+    Saves voice recording data submitted by a user.
+    '''
+    try:
+        # get request data
+        req_data = request.get_json()
+
+        now = datetime.datetime.now()
+        mood = req_data['mood']     #required
+        audio = req_data['audio']   #required
+        name = req_data['name']
+        age = req_data['age']
+        location = req_data['location']
+
+        # connect to db
+        db_conn = connect_to_database()
+
+        # form query string with optional fields
+        query = 'INSERT INTO messages (message_date, user_mood, user_name, user_age, user_location) \
+                VALUES ({}, {}'.format(now, mood)
+
+        if name:
+            query += ', "{}"'.format(name)
+        else:
+            query += ', null'
+
+        if age:
+            query += ', {}'.format(age)
+        else:
+            query += ', null'
+
+        if location:
+            query += ', "{}"'.format(location)
+        else:
+            query += ', null'
+
+        query += ')'
+
+        print('TEST: {}'.format(query))
+
+        result = execute_query(db_conn, query)
+
+        return redirect('/')
+    except Exception as e:
+        print('Error: Trying to post new voice message to timeline: {}'.format(e))
